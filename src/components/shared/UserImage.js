073174d9217ccc3d7c9027c89aadf4f2deb2c56a
GeorgeCr <georgecraciun1998@gmail.com>
2019-07-30 22:48:23 +0300

    Add basic style and images/icons for Tweet component

--- src/components/shared/UserImage.js
+++ src/components/shared/UserImage.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './style/style.scss';
 
 function UserImage(props) {
     return (
-        <img src="https://images.unsplash.com/photo-1458071103673-6a6e4c4a3413?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="geo" />
+        <div className="user-image">
+            <img src={require('../../assets/img/img_avatar.png')} alt="geo" />
+        </div>
     );
 }
+
+// woman: https://www.w3schools.com/howto/img_avatar2.png
+
+// https://images.unsplash.com/photo-1458071103673-6a6e4c4a3413?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60
 
 export default UserImage;
